8b753cb993510bc19c768a8eba7cb6c16c6ac6f5
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-01-17 21:00:53 +1000

    connect login front with backend

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,13 +2,40 @@ import styles from "./Login.module.css"
 import {NavLink} from "react-router-dom";
 import logo from "../../images/logo.svg";
 
+function onSubmit(event) {
+    event.preventDefault();
+    const data = {
+        username: event.target.elements.name.value,
+        password: event.target.elements.password.value
+    }
+    fetch('http://localhost:8000/api/login', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => {
+            if (res.status === 200) {
+                res.json().then((res) => {
+                    console.log(res);
+                    localStorage.setItem("token", res.token);
+                })
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            alert(err)
+        });
+}
+
 function Login() {
     return (
         <div className={styles.login}>
             <NavLink to="/home" className={styles.login__logo}>
-                <img src={logo} className={styles.login__logo}/>
+                <img src={logo} className={styles.login__logo} alt={'logo'}/>
             </NavLink>
-            <form className={styles.login__form}>
+            <form className={styles.login__form} onSubmit={onSubmit}>
                 <input
                     placeholder="Введите логин"
                     type="text"
