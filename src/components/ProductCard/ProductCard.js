1fbfe279252d14f2e4f24023291273123fd91401
BornOfTheMoon <mun4444@yandex.ru>
2022-01-18 15:59:51 +1000

    little styled menu page

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,33 +1,23 @@
 import styles from "./ProductCard.module.css"
-import {NavLink} from "react-router-dom";
 import {useNavigate} from "react-router";
 
 function ProductCard({product}) {
-    function routing() {
-        console.log(product.name)
-        return (
-            <NavLink to="/home"/>
-        )
-    }
-
     const navigate = useNavigate();
 
     return (
         <div className={styles.productCard}>
+            <img src={product.image} alt="avatar"/>
             <div className={styles.productCard__content}>
-                <img src={product.image}/>
                 <div className={styles.productCard__content_info}>
                     <p>Название: {product.name}</p>
                     <p>Описание: {product.description}</p>
                     <p>Цена: {product.price}</p>
                 </div>
+                <button type="submit" className={styles.productCard__content_button}
+                        onClick={async () =>{navigate(`/product:${product.name}`)}}>
+                    Подробнее
+                </button>
             </div>
-
-            //TO DO переадресация на страницу товара с прокидыванием в props товара
-            <button type="submit" className={styles.productCard__button}
-                    onClick={async event =>{navigate(`/product:${product.name}`)}}>
-                Подробнее
-            </button>
         </div>
     )
 }
